6fb26dcbb0770b41bb55f5c12652bf4234b73ab5
jedmacapinlac <dmacapin@purdue.edu>
2026-02-27 17:01:14 -0500

    added GET and POST for check ins, default expire to 10 seconds for demo (temp)

--- backend/src/routes/buildings.js
+++ backend/src/routes/buildings.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Building = require('../models/Building');
 const Room = require('../models/Room');
+const CheckIn = require('../models/CheckIn');
 
 // GET /api/buildings — all buildings sorted alphabetically
 router.get('/', async (req, res) => {
@@ -50,6 +51,55 @@ router.get('/:id/rooms', async (req, res) => {
         }
         console.error('Error fetching rooms:', err);
         res.status(500).json({ error: 'Failed to fetch rooms' });
+    }
+});
+
+// GET /api/buildings/:id/rooms/:roomId/checkins — all checkins for a room in a building
+router.get('/:id/rooms/:roomId/checkins', async (req, res) => {
+    try {
+        const building = await Building.findById(req.params.id);
+        if (!building) {
+            return res.status(404).json({ error: 'Building not found' });
+        }
+        const room = await Room.findById(req.params.roomId);
+        if (!room) {
+            return res.status(404).json({ error: 'Room not found' });
+        }
+        const checkins = await CheckIn.find({roomId: req.params.roomId, expiresAt: {$gt: new Date()}});
+        
+        res.json(checkins);
+
+    } catch (err) {
+        if (err.name === 'CastError') {
+            return res.status(404).json({ error: 'Invalid ID provided' });
+        }
+        console.error('Error fetching checkins:', err);
+        res.status(500).json({ error: 'Failed to fetch checkins' });
+    }
+});
+
+// POST /api/buoldings/:id/rooms/:roomId/checkins - create a checkin for a room
+router.post('/:id/rooms/:roomId/checkins', async (req, res) => {
+    try {
+    const building = await Building.findById(req.params.id);
+        if (!building) {
+            return res.status(404).json({ error: 'Building not found' });
+        }
+        const room = await Room.findById(req.params.roomId);
+        if (!room) {
+            return res.status(404).json({ error: 'Room not found' });
+        }
+        const checkin = new CheckIn({buildingId: req.params.id, roomId: req.params.roomId, expiresAt: Date.now() + 10 * 1000});
+        await checkin.save();
+        room.currentOccupancy++;
+        await room.save();
+        res.status(201).json(checkin)
+    } catch (err) {
+        if (err.name === 'CastError') {
+            return res.status(404).json({ error: 'Invalid ID provided' });
+        }
+        console.error('Error fetching checkins:', err);
+        res.status(500).json({ error: 'Failed to fetch checkins' });
     }
 });
 
