d1ad20e29f03e78fff1a83cc47b812ea76e84224
jedmacapinlac <dmacapin@purdue.edu>
2026-02-27 14:00:29 -0500

    schema for checkin

---
+++ backend/src/models/CheckIn.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const checkInSchema = new mongoose.Schema(
+    {
+        buildingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Building',
+            required: [true, 'Building reference is required'],
+        },
+        roomId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Room',
+            required: [true, 'Room reference is required'],
+        },
+        expiresAt: {
+          type: Date,
+          required: [true, 'Expire time is required'],
+        },
+    },
+    { timestamps: true }
+);
+module.exports = mongoose.model('CheckIn', checkInSchema);
