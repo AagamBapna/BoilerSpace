a9963aedc52592b539fe02ef4f2927093abdcd2e
Aagam Bapna <aagambapna@gmail.com>
2026-02-28 00:18:39 -0500

    bcrypt for passwords

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -39,5 +40,11 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema);
