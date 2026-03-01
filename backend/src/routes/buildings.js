8063693211db3406bd6801087c823940c2e24bc4
jedmacapinlac <dmacapin@purdue.edu>
2026-03-01 11:20:04 -0500

    remove check in stuff from building route

--- backend/src/routes/buildings.js
+++ backend/src/routes/buildings.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Building = require('../models/Building');
 const Room = require('../models/Room');
-const CheckIn = require('../models/CheckIn');
-const { protect } = require('../middleware/auth');
-const User = require('../models/User');
-
 
 // GET /api/buildings — all buildings sorted alphabetically
 router.get('/', async (req, res) => {
@@ -54,85 +50,6 @@ router.get('/:id/rooms', async (req, res) => {
         }
         console.error('Error fetching rooms:', err);
         res.status(500).json({ error: 'Failed to fetch rooms' });
-    }
-});
-
-// GET /api/buildings/:id/rooms/:roomId/checkins — all checkins for a room in a building
-router.get('/:id/rooms/:roomId/checkins', async (req, res) => {
-    try {
-        const building = await Building.findById(req.params.id);
-        if (!building) {
-            return res.status(404).json({ error: 'Building not found' });
-        }
-        const room = await Room.findById(req.params.roomId);
-        if (!room) {
-            return res.status(404).json({ error: 'Room not found' });
-        }
-        const checkins = await CheckIn.find({roomId: req.params.roomId, expiresAt: {$gt: new Date()}});
-        res.json(checkins);
-    } catch (err) {
-        if (err.name === 'CastError') {
-            return res.status(404).json({ error: 'Invalid ID provided' });
-        }
-        console.error('Error fetching checkins:', err);
-        res.status(500).json({ error: 'Failed to fetch checkins' });
-    }
-});
-
-// POST /api/buildings/:id/rooms/:roomId/checkins - create a checkin for a room
-router.post('/:id/rooms/:roomId/checkins',protect,  async (req, res) => {
-    try {
-        const building = await Building.findById(req.params.id);
-        if (!building) {
-            return res.status(404).json({ error: 'Building not found' });
-        }
-        const room = await Room.findById(req.params.roomId);
-        if (!room) {
-            return res.status(404).json({ error: 'Room not found' });
-        }
-        const checkin = await CheckIn.findOne({buildingId: req.params.id, roomId: req.params.roomId, expiresAt:{$gt: new Date()}, userId: req.user._id});
-        if (!checkin) {
-            const new_checkin = new CheckIn({buildingId: req.params.id, roomId: req.params.roomId, expiresAt: new Date(Date.now() + 10 * 1000) ,userId: req.user._id});
-            await new_checkin.save();
-            room.currentOccupancy++;
-            await room.save();
-            return res.status(201).json(new_checkin);
-        }
-        return res.status(409).json({error: 'You are already checked into this room'});
-    } catch (err) {
-        if (err.name === 'CastError') {
-            return res.status(404).json({ error: 'Invalid ID provided' });
-        }
-        console.error('Error fetching checkins:', err);
-        res.status(500).json({ error: 'Failed to fetch checkins' });
-    }
-});
-
-// DELETE /api/buildings/:id/rooms/:roomId/checkins — allow for users to checkout
-router.delete('/:id/rooms/:roomId/checkins/:checkinID', async (req, res) => {
-    try {
-        const building = await Building.findById(req.params.id);
-        if (!building) {
-            return res.status(404).json({ error: 'Building not found' });
-        }
-        const room = await Room.findById(req.params.roomId);
-        if (!room) {
-            return res.status(404).json({ error: 'Room not found' });
-        }
-        const checkin = await CheckIn.findById(req.params.checkinID);
-        if (!checkin) {
-            return res.status(404).json({ error: 'Checkin not found' });
-        }
-        await checkin.deleteOne();
-        room.currentOccupancy--;
-        await room.save()
-        res.status(204).send();
-    } catch (err) {
-        if (err.name === 'CastError') {
-            return res.status(404).json({ error: 'Invalid ID provided' });
-        }
-        console.error('Error deleting checkin:', err);
-        res.status(500).json({ error: 'Failed to fetch checkins' });
     }
 });
 
