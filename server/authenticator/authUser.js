46a973da7e81aa2e2a12f0869a0069f11899fb30
bidit-acharyya <105981837+bidit-acharyya@users.noreply.github.com>
2026-02-28 21:41:32 -0500

    added POST functionality so that user can update course list

--- server/authenticator/authUser.js
+++ server/authenticator/authUser.js
@@ -1,15 +1,15 @@
 const { users } = require('../data');
 
-function authUser(request, response, next) {
-    const userId = request.headers['x-user-id'];
+function authUser(req, resp, next) {
+    const userId = req.headers['x-user-id'];
     if (!userId) {
-        return response.status(401).json({error: 'Unauthorized. Please log in.'});
+        return resp.status(401).json({error: 'Unauthorized. Please log in.'});
     }
     const user = users.find((u) => u._id === userId);
     if (!user) {
-        return response.status(401).json({error: 'Unauthorized. Invalid user.'});
+        return resp.status(401).json({error: 'Unauthorized. Invalid user.'});
     }
-    request.user = user;
+    req.user = user;
     next();
 }
 module.exports = { authUser };
