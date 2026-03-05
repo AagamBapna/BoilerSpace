e4991bf81456d478d8e09477ed09ee325bf375ba
bidit-acharyya <105981837+bidit-acharyya@users.noreply.github.com>
2026-03-05 17:44:12 -0500

    made temporary note schema so I can work on my user story

---
+++ backend/src/models/Note.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+    numVotes: { type: Number, default: 0 },
+    votes: [{
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      voteType: { type: String, enum: ['upvote', 'downvote'], required: true },
+    }],
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Note', noteSchema);
