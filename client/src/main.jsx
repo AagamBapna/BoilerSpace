36ab5a23e38829b1bc6c93059979e5fbca95f7a8
jedmacapinlac <dmacapin@purdue.edu>
2026-04-02 11:01:57 -0400

    added LocationProvider into main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { LocationProvider } from './contexts/LocationContext.jsx'
+import LocationPromptModal from './components/LocationPromptModal.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <LocationProvider>
+        <LocationPromptModal />
+        <App />
+      </LocationProvider>
     </BrowserRouter>
   </StrictMode>,
 )
